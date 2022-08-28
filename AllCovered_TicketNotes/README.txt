18e631f03f99540433677cb8bc46b1928e33b74c
Clement Chieh <charliec12@users.noreply.github.com>
2022-08-27 22:34:22 -0400

    adding functionality to the copy button
    incorporated jquery to aid with copy button functionality

--- AllCovered_TicketNotes/README.txt
+++ AllCovered_TicketNotes/README.txt
@@ -4,6 +4,7 @@ Purpose : create a web app that would easily copy, export and save ticket entrie
 
 8/20/2022 - completed design of web app.
 8/21/2022 - New Entry Button working partially,
+8/27/2022 - Copy button working to copy all entries of the form to clipboard
 
 
 
@@ -16,5 +17,5 @@ Buttons - Add functionality to New Entry, Copy and export.
 
 Button Functions
 New Entry - save filled form to csv file then clear entries
-Copy - copy all entries to clipboard
+Copy - copy all entries to clipboard - DONE
 Export - export a copy of the CSV file to user preferred location
--- AllCovered_TicketNotes/script/index.js
+++ AllCovered_TicketNotes/script/index.js
@@ -1,22 +1,72 @@
 var newEntryButton = document.getElementById("newEntry");
 var copyEntryButton = document.getElementById("copyEntry");
 var exportEntryButton = document.getElementById("exportEntry");
-
 var ticketForm = document.getElementById("acTicket");
 
-
-function clearForm(){
+function clearForm() {
   //1: TODO: copy and append entries to csv file
 
-  //2: clear and reset the form. DONE 
+  //2: clear and reset the form. DONE
   ticketForm.reset();
 }
 
-function displayMes(){
-  alert("bui");
+function copyToClipBoard() {
+  var contactName = $("#contact-name").val();
+  var contactMethod = $("#contact-method").val();
+  var issueSummary = $("#issue-summary").val();
+  var deviceType = $("#device-type option:selected").text();
+  var deviceName = $("#device-name").val();
+  var deviceOs = $("#device-os").val();
+  var deviceIp = $("#device-ip").val();
+  var relatedDeviceType1 = $("#related-device-type-1 option:selected").text();
+  var relatedDeviceType2 = $("#related-device-type-2 option:selected").text();
+  var relatedDeviceName1 = $("#related-device-name-1").val();
+  var relatedDeviceName2 = $("#related-device-name-2").val();
+  var relatedDeviceOs1 = $("#related-device-os-1").val();
+  var relatedDeviceOs2 = $("#related-device-os-2").val();
+  var relatedDeviceIp1 = $("#related-device-ip-1").val();
+  var relatedDeviceIp2 = $("#related-device-ip-2").val();
+  var stepsTaken = $("#steps-taken").val();
+
+  navigator.clipboard.writeText(
+    "Contact Details" +
+    "\n\n" +
+    "Contact Name : " + contactName + "\n" +
+    "Contact Method : " + contactMethod + "\n" +
+    "Issue Summary : " + issueSummary + "\n" +
+    "\n\n" +
+    "Client Device Info" +
+    "\n\n" +
+    "Device Type : " + deviceType + "\n" +
+    "Device Name : " + deviceName + "\n" +
+    "Device OS : " + deviceOs + "\n" +
+    "Device IP : " + deviceIp + "\n" +
+    "\n\n" +
+    "Related Device" +
+    "\n\n" +
+    "Device Type : " + relatedDeviceType1 + "\n" +
+    "Device Name : " + relatedDeviceName1 + "\n" +
+    "Device OS : " + relatedDeviceOs1 + "\n" +
+    "Device IP : " + relatedDeviceIp1 + "\n" +
+    "\n\n" +
+    "Related Device" +
+    "\n\n" +
+    "Device Type : " + relatedDeviceType2 + "\n" +
+    "Device Name : " + relatedDeviceName2 + "\n" +
+    "Device OS : " + relatedDeviceOs2 + "\n" +
+    "Device IP : " + relatedDeviceIp2 + "\n" +
+    "\n\n" +
+    "Troubleshooting Steps" +
+    "\n" +
+    stepsTaken + "\n"
+
+  );
+}
+
+function displayMes() {
+  alert("iextract");
 }
 
 newEntryButton.addEventListener("click", clearForm);
-
-copyEntryButton.addEventListener("click", displayMes);
+copyEntryButton.addEventListener("click", copyToClipBoard);
 exportEntryButton.addEventListener("click", displayMes);
