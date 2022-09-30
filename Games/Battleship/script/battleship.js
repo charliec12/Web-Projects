73844bba8bc9aec8d6868996d4d2cf17fc967aa4
Clement Chieh <charliec12@users.noreply.github.com>
2022-09-30 17:30:52 -0400

    starting battleship game

---
+++ Games/Battleship/script/battleship.js
@@ -0,0 +1,14 @@
+let location_1 = 3;
+let location_2 = 4;
+let location_3 = 5;
+
+let guess;
+let hits = 0;
+let guesses = 0;
+
+let isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt("Ready, aim, Fiah! (Enter a number 0-6) : ");
+    
+}
