334bf5f37bdd8272621a14e7a7138f70ad14b3e8
Clement Chieh <charliec12@users.noreply.github.com>
2023-07-18 16:48:38 -0400

    Update script.js

--- Weather_WebApp/script.js
+++ Weather_WebApp/script.js
@@ -3,7 +3,7 @@ var description = $("#description");
 var temperature = $("#temp");
 var zipBtn = $("#weather_by_zip");
 var locBtn = $("#weather_by_location");
-var apiKey = "e5e7df51a0d038b790c4ba6306cbe477"
+var apiKey = ""
 
 zipBtn[0].addEventListener("click",function(){
     fetch("https://api.openweathermap.org/data/2.5/weather?id="+cityZip+"&appid="+apiKey+"").then(res => res.json())
