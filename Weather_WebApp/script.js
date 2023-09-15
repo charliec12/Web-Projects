a5ed25e41418603ba11e79c4abdf7bf98a7d1158
Clement Chieh <charliec12@users.noreply.github.com>
2023-09-15 17:46:46 -0400

    clearing api key

--- Weather_WebApp/script.js
+++ Weather_WebApp/script.js
@@ -3,7 +3,7 @@ var description = $("#description");
 var temperature = $("#temp");
 var zipBtn = $("#weather_by_zip");
 var locBtn = $("#weather_by_location");
-var apiKey = "e5e7df51a0d038b790c4ba6306cbe477"
+var apiKey = ""
 
 function getInputValue(){
 var city = document.getElementById('zipCode').value;
